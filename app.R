ddaaa7dd8054f4e1b58d8d47bc729b1b6dac0a1a
Daniel R. Williams <danrwill@me.com>
2024-02-21 15:55:51 -0500

    updated folder structure

--- app.R
+++ app.R
@@ -93,6 +93,7 @@ server <- function(input, output) {
 
   output$estTable <- renderTable({
 
+    # TODO ad handeling for "feet" and "\'"
     # calculate area if given dimentions
     input$dimentions |> str_squish() |> req()
     area <- if (is.numeric(input$dimentions)) {input$dimentions}
